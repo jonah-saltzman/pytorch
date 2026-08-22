257418d71f5c42e21382bc41cfde3a3f7765fee2
Z Liu <zhuojin.liu@amd.com>
2026-08-22 17:08:25 -0400

    test nproc/4

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -218,7 +218,7 @@ if [[ -z "${OMP_NUM_THREADS:-}" ]] && [[ -n "${USE_ARC:-}" ]]; then
 fi
 
 if [[ "$BUILD_ENVIRONMENT" == *rocm* ]]; then
-  export OMP_NUM_THREADS=8
+  export OMP_NUM_THREADS=$(( $(nproc) / 4 ))
 fi
 
 export LANG=C.UTF-8
